715d2c56be5e275b031b6b965c035b0e4d169390
Kusum Agrawal <kusum.agrawal.0202@gmail.com>
2025-02-15 00:03:10 +0530

    define flask app

---
+++ student_portal/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "secret" # change this production
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import User
+
+#home page
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+#register page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['user   name']
+        email = request.form['email']
+        password = request.form['password']
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+#Login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['user'] = user.username
+            return redirect(url_for('profile'))
+        else:
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+#profile page
+@app.route('/profile')
+def profile():
+    if 'user' in session:
+        user = User.query.filter_by(username=session['user']).first()
+        return render_template('profile.html', user=user)
+    return redirect(url_for('login'))
+
+#logout
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
